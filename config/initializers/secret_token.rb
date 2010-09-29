292452e47bf539c5a397701beec0a157cfe639dd
Dave Doolin <david.doolin@gmail.com>
2010-09-29 16:54:35 -0700

    Trying to merge branch.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cacklist::Application.config.secret_token = '6950fc671fb7dad3fa159f9d30917a330a43493f9750bdc07d6723ce10004154f8e3f8756ceaa0afb7a95fbe815805eaf05fd816dea4f334014556d287cb4111'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_cacklist_session',
-  :secret      => '446b08b16e122fddc266da6c93781d9c7b69a4fe95a271a0c61275e3167b20b612319798e0c14ee8860b60b5d26b60ac0c53d4b32e8cb3d523218422d412764f'
-}
+Cacklist::Application.config.session_store :cookie_store, :key => '_cacklist_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Cacklist::Application.config.session_store :active_record_store
