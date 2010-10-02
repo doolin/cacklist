7a8444c0792d932390125187f1ad3fe86c134691
Dave Doolin <david.doolin@gmail.com>
2010-10-01 17:27:22 -0700

    Basic app runs with mysql and in rails 3.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Cacklist::Application.routes.draw do
+
   get "pages/home"
+  #root :to => "home#index"
 
   get "pages/contact"
+
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
